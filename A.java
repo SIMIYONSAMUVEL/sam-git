1c1b2e4615aacfe70c4f8a2168323bdc9b619215
unknown <simiyon2807@gmail.com>
2022-02-10 21:57:52 +0530

    first commit

---
+++ A.java
@@ -0,0 +1,39 @@
+package org.sam;
+
+public class A {
+
+	public static void main(String[] args) {
+		
+	for (int i = 1; i <=50; i++) {
+
+	for (int j = 1; j <=50; j++) {
+		if (i%2==0) {
+			System.out.println("even number:"+i);
+			if (j%2==0) {
+				System.out.println("even number:"+j);
+				
+			}
+			
+		}
+		else {
+			System.out.println("odd number:"+i);
+			System.out.println("odd number:"+j);
+		}
+			
+			
+		}
+		
+			
+		}
+		
+	}
+			
+		}
+		
+	
+	
+	
+	
+	
+	
+
